977f63369cf6954c51be48ea37ac47c75f7e6877
Dylan Hammer <hammer.dylan@gmail.com>
2022-05-31 11:04:10 -0700

    testing gtm functionality

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -30,7 +30,15 @@ const MyApp = ({ Component, pageProps }) => {
 		<ThemeProvider theme={theme}>
 		<div className="main-container">
 			<Script type="text/javascript" src="https://xl1.pop6serve.com/popsixle.php?t=5fd8d26899c79a2e031ac156a64424166b771901c42eb88c02ca4983d7e09e03"/>
-
+			<Script
+				id="gtm_script"
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{ 
+					__html: `
+						(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WFQB7WV'); 
+					`,
+				}}
+			/>
 			<HammerCursor />
 			<Head>
 				<title>Hammer Time</title>
@@ -50,6 +58,7 @@ const MyApp = ({ Component, pageProps }) => {
 				/>
 			</Head>
 			<Layout>
+			<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WFQB7WV"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 				<Component {...pageProps} />
 			</Layout>
 		</div>
